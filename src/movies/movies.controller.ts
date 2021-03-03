554906806511aca17d61e39b96efa52ee45a2420
wizzy <kluverdev@gmail.com>
2021-03-02 21:59:31 -0400

    more routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,21 +1,43 @@
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('movies')
+@Controller('movies') //this is more like localhost:3000/movies
 export class MoviesController {
   @Get()
   getAll() {
     return `this is all movies`;
   }
-
-  @Get('/:id')
+  @Get('search')
+  search(@Query("year") searchingYear: string) {
+    return `we are searching for this year ${searchingYear}`;
+  }
+  @Get(':id')
   getOne(@Param('id') id: string) {
     return `this is all movies with the id ${id}`;
   }
 
-  @Post()
-  create() {
-    return `create id`;
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
+  }
+  //you need to call the body if you want the body. same applies to the parameters
 
   @Delete('/:id')
   remove(@Param('id') id: string) {
