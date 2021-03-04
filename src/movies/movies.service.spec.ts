754e03dbf115b9ff1c40468621f5a00080f3569c
wizzy <kluverdev@gmail.com>
2021-03-04 11:11:57 -0400

    testing getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,29 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  describe('getAll', () => {
+    it('should be an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'net 5',
+        year: 20042,
+        genres: ['action', 'kungfu'],
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
+    it('should throw a 404 error', () => {
+      try {
+        service.getOne(1555);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
